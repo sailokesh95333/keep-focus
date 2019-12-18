2389a3dba3cbbb74bdf969390443d9861a188698
Nico Haenggi <nico@haenggi.ch>
2019-12-18 22:28:31 +0100

    added password protection

--- src/api/webhooks.ts
+++ src/api/webhooks.ts
@@ -2,6 +2,7 @@
 // sets up all the Webhook API methods
 
 // import dependencies
+import * as crypto from 'crypto';
 import { manager } from '../sync-manager';
 
 /** Webhook API Routes
@@ -21,11 +22,29 @@ const lametric = function lametric(options: any, object: any) : any {
 }
 
 const togglePunishment = function togglePunishment(options: any, object: any) : any {
+  if (manager.db.punishmentIsActive) {
+    // trying to disable punishment
+    if (!options.password) {
+      return {
+        success: false,
+        punishmentIsActive: manager.db.punishmentIsActive,
+        message: 'tried to disable punishment without providing password'
+      }
+    }
+
+    // make sure the hash is equal to the password
+    const hashed = crypto.createHash('sha256').update(options.password, 'utf8').digest('hex');
+    if (hashed !== manager.config.passwordHash) {
+      return {
+        success: false,
+        punishmentIsActive: manager.db.punishmentIsActive,
+        message: 'tried to disable punishment with incorrect password'
+      }
+    }
+  }
+  
   // trigger display both property
   manager.db.punishmentIsActive = !manager.db.punishmentIsActive;
-  
-  // check current status and require password if want to disable
-  console.log(options.password);
 
   // return response object
   return {
